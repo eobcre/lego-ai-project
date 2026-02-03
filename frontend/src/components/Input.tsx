3349c48a3ac84b03c3bee0aa8a87dbed3df56717
Emmy Manning <emswoo@EM.local>
2026-02-03 15:19:47 -0500

    added a button in input

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,11 +1,17 @@
 import { useState } from 'react';
+import { Icon } from '@iconify/react';
 
 const Input = () => {
   const [value, setValue] = useState('');
 
-  console.log(value)
-
-  return <input type='text' value={value} onChange={(e) => setValue(e.target.value)} placeholder='Type a command (e.g forward)' className='border px-3 py-2 rounded' />;
+  return (
+    <div className='relative'>
+      <input type='text' value={value} onChange={(e) => setValue(e.target.value)} placeholder='Type a command (e.g forward)' className='border px-3 py-2 rounded w-full' />
+      <button className='absolute right-2 top-1/2 -translate-y-1/2 cursor-pointer hover:opacity-60'>
+        <Icon icon='material-symbols:send' width='24' height='24' />
+      </button>
+    </div>
+  );
 };
 
 export default Input;
