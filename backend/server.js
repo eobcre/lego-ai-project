35fbab79efb2424ad0f02c10d00334f040e0f4f9
eobcre <88697509+eobcre@users.noreply.github.com>
2026-02-13 03:01:28 +0900

    server, router and lego bluetooth boot

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,36 @@
 import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+import { connectTrain } from "./connectTrain.js";
+import { nlpRouter } from "./routes/nlp.js";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT;
+app.use(cors({ origin: process.env.CLIENT_URL }));
+app.use(express.json());
 
+// server
 const server = app.listen(PORT, () => {
-  console.log("Server is running.");
+  console.log(`Server is running on port ${PORT}.`);
 });
+
+// router
+app.use(
+  "/api",
+  nlpRouter(() => train),
+);
+
+// boot lego bluetooth
+let train;
+
+const bootLego = async () => {
+  try {
+    train = await connectTrain({ portName: "B" });
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+};
+
+bootLego();
