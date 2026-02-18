cf57a0c88e1c0e6cc67bd3b7f6c2e4634ebbb8c8
eobcre <88697509+eobcre@users.noreply.github.com>
2026-02-19 05:15:28 +0900

    bedrock integrate test

--- backend/routes/nlp.js
+++ backend/routes/nlp.js
@@ -1,24 +1,21 @@
 import { Router } from "express";
+import { bedrock } from "../services/bedrock.js";
 
 export const nlpRouter = () => {
   const router = Router();
 
   router.post("/nlp", async (req, res) => {
-    res.json({
-      ok: true,
-      message: "You made it!"
-    })
+    try {
+      const text = req.body?.text.trim();
+      if (!text) {
+        return res.status(400).json({ ok: false, error: "Text is required." });
+      }
+      const answer = await bedrock(text);
+      return res.json({ ok: true, answer });
+    } catch (e) {
+      return res.status(500).json({ ok: false, error: "Error." });
+    }
   });
 
   return router;
 };
-
-
-
-
-
-
-
-
-
-
--- backend/services/bedrock.js
+++ backend/services/bedrock.js
@@ -7,3 +7,18 @@ import {
 const client = new BedrockRuntimeClient({
   region: process.env.AWS_REGION,
 });
+
+export async function bedrock(text) {
+  const out = await client.send(
+    new ConverseCommand({
+      modelId: process.env.MODEL_ID,
+      messages: [{ role: "user", content: [{ text }] }],
+    }),
+  );
+
+  const answer = out?.output?.message?.content[0].text;
+  // console.log("answer:", answer);
+  console.log("out:", out);
+
+  return answer;
+}
