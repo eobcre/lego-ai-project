e9b972b783d0c83dc68596cfce64f9d61dd33125
eobcre <88697509+eobcre@users.noreply.github.com>
2026-02-19 04:03:09 +0900

    created bedrock

---
+++ backend/services/bedrock.js
@@ -0,0 +1,9 @@
+import "dotenv/config";
+import {
+  BedrockRuntimeClient,
+  ConverseCommand,
+} from "@aws-sdk/client-bedrock-runtime";
+
+const client = new BedrockRuntimeClient({
+  region: process.env.AWS_REGION,
+});
