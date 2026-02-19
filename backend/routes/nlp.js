94b2e010db639050fdd2ae637aac79a592426c3e
eobcre <88697509+eobcre@users.noreply.github.com>
2026-02-20 03:17:03 +0900

    bedrock returns json format test

--- backend/routes/nlp.js
+++ backend/routes/nlp.js
@@ -1,19 +1,35 @@
 import { Router } from "express";
 import { bedrock } from "../services/bedrock.js";
 
+/* 
+bedrock returns json format test
+*/
+
 export const nlpRouter = () => {
   const router = Router();
 
   router.post("/nlp", async (req, res) => {
+    // user text validation
     try {
       const text = req.body?.text.trim();
       if (!text) {
         return res.status(400).json({ ok: false, error: "Text is required." });
       }
+
+      // bedrock process
       const answer = await bedrock(text);
-      return res.json({ ok: true, answer });
-    } catch (e) {
-      return res.status(500).json({ ok: false, error: "Error." });
+
+      // parse bedrock answer
+      try {
+        const answerParsed = JSON.parse(answer);
+        return res.json({ ok: true, answerParsed, answer });
+      } catch (err) {
+        console.error(err);
+        return res.status(500).json({ ok: false });
+      }
+      
+    } catch (err) {
+      console.error("NLP bedrock error:", err);
     }
   });
 
