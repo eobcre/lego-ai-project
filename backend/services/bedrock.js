0302b0fa28091244d6c34b259532afdebc05f013
eobcre <88697509+eobcre@users.noreply.github.com>
2026-02-20 03:17:57 +0900

    bedrock returns json format test

--- backend/services/bedrock.js
+++ backend/services/bedrock.js
@@ -4,6 +4,11 @@ import {
   ConverseCommand,
 } from "@aws-sdk/client-bedrock-runtime";
 
+/* 
+bedrock returns json format test
+*/
+
+// aws region
 const client = new BedrockRuntimeClient({
   region: process.env.AWS_REGION,
 });
@@ -12,13 +17,29 @@ export async function bedrock(text) {
   const out = await client.send(
     new ConverseCommand({
       modelId: process.env.MODEL_ID,
-      messages: [{ role: "user", content: [{ text }] }],
+      system: [
+        {
+          text:
+            "You control a LEGO train.\n" +
+            "Given the user input, reply ONLY with valid JSON in this exact format:\n" +
+            '{"action":"forward"} or {"action":"backward"} or {"action":"stop"}\n' +
+            "Allowed actions: forward, backward, stop.\n" +
+            "No explanation. No markdown. No extra keys.",
+        },
+      ],
+      messages: [
+        {
+          role: "user",
+          content: [{ text }],
+        },
+      ],
     }),
   );
 
+  // extract bedrock answer
   const answer = out?.output?.message?.content[0].text;
   // console.log("answer:", answer);
-  console.log("out:", out);
+  // console.log("out:", out);
 
   return answer;
 }
